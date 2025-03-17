b05531aba5795d42f720839049a3c084380b714f
Guillaume Petiot <hello@guillaumepetiot.com>
2025-03-17 16:30:37 +0000

    Test: use correct colors

--- src/__tests__/utils/wheelUtils.test.js
+++ src/__tests__/utils/wheelUtils.test.js
@@ -1,4 +1,5 @@
 import { calculateWheelSlices } from '../../utils/wheelUtils';
+import { COLORS } from '../../utils/constants';
 
 describe('wheelUtils', () => {
   describe('calculateWheelSlices', () => {
@@ -25,12 +26,19 @@ describe('wheelUtils', () => {
       expect(firstAngle).toBe(0);
     };
 
+    const color1 = COLORS[0];
+    const color2 = COLORS[1];
+    const color3 = COLORS[2];
+    const color4 = COLORS[3];
+    const color5 = COLORS[4];
+    const color6 = COLORS[5];
+
     it('should generate correct slices for 1 choice', () => {
       const wheelData = createWheelData(['Choice 1']);
       const slices = calculateWheelSlices(wheelData);
 
       expect(slices).toEqual([
-        { text: 'Choice 1', color: 'color1', rotate: 0, sliceAngle: 360 }
+        { text: 'Choice 1', color: color1, rotate: 0, sliceAngle: 360 }
       ]);
       verifySliceOrder(slices);
     });
@@ -40,10 +48,10 @@ describe('wheelUtils', () => {
       const slices = calculateWheelSlices(wheelData);
 
       expect(slices).toEqual([
-        { text: "Choice 1", color: "color1", rotate: 0, sliceAngle: 90 },
-        { text: "Choice 2", color: "color2", rotate: 90, sliceAngle: 90 },
-        { text: "Choice 1", color: "color1", rotate: 180, sliceAngle: 90 },
-        { text: "Choice 2", color: "color2", rotate: 270, sliceAngle: 90 },
+        { text: "Choice 1", color: color1, rotate: 0, sliceAngle: 90 },
+        { text: "Choice 2", color: color2, rotate: 90, sliceAngle: 90 },
+        { text: "Choice 1", color: color1, rotate: 180, sliceAngle: 90 },
+        { text: "Choice 2", color: color2, rotate: 270, sliceAngle: 90 },
       ]);
       verifySliceOrder(slices);
     });
@@ -53,12 +61,12 @@ describe('wheelUtils', () => {
       const slices = calculateWheelSlices(wheelData);
 
       expect(slices).toEqual([
-        { text: "Choice 1", color: "color1", rotate: 0, sliceAngle: 60 },
-        { text: "Choice 2", color: "color2", rotate: 60, sliceAngle: 60 },
-        { text: "Choice 3", color: "color3", rotate: 120, sliceAngle: 60 },
-        { text: "Choice 1", color: "color1", rotate: 180, sliceAngle: 60 },
-        { text: "Choice 2", color: "color2", rotate: 240, sliceAngle: 60 },
-        { text: "Choice 3", color: "color3", rotate: 300, sliceAngle: 60 },
+        { text: "Choice 1", color: color1, rotate: 0, sliceAngle: 60 },
+        { text: "Choice 2", color: color2, rotate: 60, sliceAngle: 60 },
+        { text: "Choice 3", color: color3, rotate: 120, sliceAngle: 60 },
+        { text: "Choice 1", color: color1, rotate: 180, sliceAngle: 60 },
+        { text: "Choice 2", color: color2, rotate: 240, sliceAngle: 60 },
+        { text: "Choice 3", color: color3, rotate: 300, sliceAngle: 60 },
       ]);
       verifySliceOrder(slices);
     });
@@ -68,14 +76,14 @@ describe('wheelUtils', () => {
       const slices = calculateWheelSlices(wheelData);
 
       expect(slices).toEqual([
-        { text: "Choice 1", color: "color1", rotate: 0, sliceAngle: 45 },
-        { text: "Choice 2", color: "color2", rotate: 45, sliceAngle: 45 },
-        { text: "Choice 3", color: "color3", rotate: 90, sliceAngle: 45 },
-        { text: "Choice 4", color: "color4", rotate: 135, sliceAngle: 45 },
-        { text: "Choice 1", color: "color1", rotate: 180, sliceAngle: 45 },
-        { text: "Choice 2", color: "color2", rotate: 225, sliceAngle: 45 },
-        { text: "Choice 3", color: "color3", rotate: 270, sliceAngle: 45 },
-        { text: "Choice 4", color: "color4", rotate: 315, sliceAngle: 45 },
+        { text: "Choice 1", color: color1, rotate: 0, sliceAngle: 45 },
+        { text: "Choice 2", color: color2, rotate: 45, sliceAngle: 45 },
+        { text: "Choice 3", color: color3, rotate: 90, sliceAngle: 45 },
+        { text: "Choice 4", color: color4, rotate: 135, sliceAngle: 45 },
+        { text: "Choice 1", color: color1, rotate: 180, sliceAngle: 45 },
+        { text: "Choice 2", color: color2, rotate: 225, sliceAngle: 45 },
+        { text: "Choice 3", color: color3, rotate: 270, sliceAngle: 45 },
+        { text: "Choice 4", color: color4, rotate: 315, sliceAngle: 45 },
       ]);
       verifySliceOrder(slices);
     });
@@ -85,16 +93,16 @@ describe('wheelUtils', () => {
       const slices = calculateWheelSlices(wheelData);
 
       expect(slices).toEqual([
-        { text: "Choice 1", color: "color1", rotate: 0, sliceAngle: 36 },
-        { text: "Choice 2", color: "color2", rotate: 36, sliceAngle: 36 },
-        { text: "Choice 3", color: "color3", rotate: 72, sliceAngle: 36 },
-        { text: "Choice 4", color: "color4", rotate: 108, sliceAngle: 36 },
-        { text: "Choice 5", color: "color5", rotate: 144, sliceAngle: 36 },
-        { text: "Choice 1", color: "color1", rotate: 180, sliceAngle: 36 },
-        { text: "Choice 2", color: "color2", rotate: 216, sliceAngle: 36 },
-        { text: "Choice 3", color: "color3", rotate: 252, sliceAngle: 36 },
-        { text: "Choice 4", color: "color4", rotate: 288, sliceAngle: 36 },
-        { text: "Choice 5", color: "color5", rotate: 324, sliceAngle: 36 },
+        { text: "Choice 1", color: color1, rotate: 0, sliceAngle: 36 },
+        { text: "Choice 2", color: color2, rotate: 36, sliceAngle: 36 },
+        { text: "Choice 3", color: color3, rotate: 72, sliceAngle: 36 },
+        { text: "Choice 4", color: color4, rotate: 108, sliceAngle: 36 },
+        { text: "Choice 5", color: color5, rotate: 144, sliceAngle: 36 },
+        { text: "Choice 1", color: color1, rotate: 180, sliceAngle: 36 },
+        { text: "Choice 2", color: color2, rotate: 216, sliceAngle: 36 },
+        { text: "Choice 3", color: color3, rotate: 252, sliceAngle: 36 },
+        { text: "Choice 4", color: color4, rotate: 288, sliceAngle: 36 },
+        { text: "Choice 5", color: color5, rotate: 324, sliceAngle: 36 },
       ]);
       verifySliceOrder(slices);
     });
@@ -104,12 +112,12 @@ describe('wheelUtils', () => {
       const slices = calculateWheelSlices(wheelData);
 
       expect(slices).toEqual([
-        { text: "Choice 1", color: "color1", rotate: 0, sliceAngle: 60 },
-        { text: "Choice 2", color: "color2", rotate: 60, sliceAngle: 60 },
-        { text: "Choice 3", color: "color3", rotate: 120, sliceAngle: 60 },
-        { text: "Choice 4", color: "color4", rotate: 180, sliceAngle: 60 },
-        { text: "Choice 5", color: "color5", rotate: 240, sliceAngle: 60 },
-        { text: "Choice 6", color: "color6", rotate: 300, sliceAngle: 60 },
+        { text: "Choice 1", color: color1, rotate: 0, sliceAngle: 60 },
+        { text: "Choice 2", color: color2, rotate: 60, sliceAngle: 60 },
+        { text: "Choice 3", color: color3, rotate: 120, sliceAngle: 60 },
+        { text: "Choice 4", color: color4, rotate: 180, sliceAngle: 60 },
+        { text: "Choice 5", color: color5, rotate: 240, sliceAngle: 60 },
+        { text: "Choice 6", color: color6, rotate: 300, sliceAngle: 60 },
       ]);
       verifySliceOrder(slices);
     });

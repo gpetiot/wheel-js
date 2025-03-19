6e5309f4d23968e8ca9a20f794cd67e42f78bf90
Guillaume Petiot <hello@guillaumepetiot.com>
2025-03-19 17:38:01 +0000

    Style(wheel): improve indicator with consistent rose color and enhanced white border

--- src/components/WheelSpinner.js
+++ src/components/WheelSpinner.js
@@ -288,7 +288,15 @@ const WheelSpinner = () => {
         {/* Wheel Section */}
         <div className="flex flex-col items-center gap-6 md:w-1/2 md:pr-8">
           <div className="relative w-full max-w-[480px] aspect-square mx-auto">
-            <div className="absolute top-1/2 right-[-16px] -translate-y-1/2 w-0 h-0 border-t-[12px] border-t-transparent border-b-[12px] border-b-transparent border-r-[24px] border-r-red-500 z-10"></div>
+            <div className="absolute top-1/2 right-[-16px] -translate-y-1/2 w-0 h-0 
+              border-t-[15px] border-t-transparent 
+              border-b-[15px] border-b-transparent 
+              border-r-[30px] border-r-rose-500
+              filter drop-shadow-md z-10
+              before:absolute before:content-[''] before:right-[-32px] before:top-[-17px] 
+              before:border-t-[17px] before:border-t-transparent 
+              before:border-b-[17px] before:border-b-transparent 
+              before:border-r-[34px] before:border-r-white/50"></div>
             <div 
               ref={wheelRef}
               className="w-full h-full rounded-full overflow-hidden relative shadow-lg will-change-transform"

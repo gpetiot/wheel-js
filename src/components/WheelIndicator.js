1534e56ee52d253e7d08ce23f0f6c0765c5b31f8
Guillaume Petiot <hello@guillaumepetiot.com>
2025-03-29 16:56:51 +0000

    Style(WheelIndicator): add white border around indicator

--- src/components/WheelIndicator.js
+++ src/components/WheelIndicator.js
@@ -1,18 +1,27 @@
-import React from 'react';
-
 const WheelIndicator = () => {
   return (
-    <div
-      className="absolute top-1/2 right-[-16px] -translate-y-1/2 w-0 h-0 
-      border-t-[15px] border-t-transparent 
-      border-b-[15px] border-b-transparent 
-      border-r-[45px] border-r-rose-500
-      filter drop-shadow-md z-10
-      before:absolute before:content-[''] before:right-[-47px] before:top-[-17px] 
-      before:border-t-[17px] before:border-t-transparent 
-      before:border-b-[17px] before:border-b-transparent 
-      before:border-r-[49px] before:border-r-rose-500/50"
-    ></div>
+    <>
+      {/* White border triangle */}
+      <div
+        className="absolute top-1/2 right-[-16px] -translate-y-1/2 w-0 h-0 
+        border-t-[19px] border-t-transparent 
+        border-b-[19px] border-b-transparent 
+        border-r-[49px] border-r-white
+        filter drop-shadow-md z-10"
+      ></div>
+      {/* Red triangle */}
+      <div
+        className="absolute top-1/2 right-[-16px] -translate-y-1/2 w-0 h-0 
+        border-t-[15px] border-t-transparent 
+        border-b-[15px] border-b-transparent 
+        border-r-[45px] border-r-rose-500
+        filter drop-shadow-md z-10
+        before:absolute before:content-[''] before:right-[-47px] before:top-[-17px] 
+        before:border-t-[17px] before:border-t-transparent 
+        before:border-b-[17px] before:border-b-transparent 
+        before:border-r-[49px] before:border-r-rose-500/50"
+      ></div>
+    </>
   );
 };
 

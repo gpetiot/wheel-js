c95fa0c4333f4d7d9e274aa6697322e7644afe53
Guillaume Petiot <hello@guillaumepetiot.com>
2025-03-29 16:53:24 +0000

    Style(WheelIndicator): update shadow color to match SpinButton theme, and increase its length

--- src/components/WheelIndicator.js
+++ src/components/WheelIndicator.js
@@ -6,12 +6,12 @@ const WheelIndicator = () => {
       className="absolute top-1/2 right-[-16px] -translate-y-1/2 w-0 h-0 
       border-t-[15px] border-t-transparent 
       border-b-[15px] border-b-transparent 
-      border-r-[30px] border-r-rose-500
+      border-r-[45px] border-r-rose-500
       filter drop-shadow-md z-10
-      before:absolute before:content-[''] before:right-[-32px] before:top-[-17px] 
+      before:absolute before:content-[''] before:right-[-47px] before:top-[-17px] 
       before:border-t-[17px] before:border-t-transparent 
       before:border-b-[17px] before:border-b-transparent 
-      before:border-r-[34px] before:border-r-white/50"
+      before:border-r-[49px] before:border-r-rose-500/50"
     ></div>
   );
 };

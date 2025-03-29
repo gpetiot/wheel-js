80ef0d0525a563d75a2d61a93b2e2ba9a9b3b7b9
Guillaume Petiot <hello@guillaumepetiot.com>
2025-03-29 16:49:19 +0000

    Refactor(components): extract wheel indicator into separate component

--- src/components/WheelSpinner.js
+++ src/components/WheelSpinner.js
@@ -5,6 +5,7 @@ import DebugPanel from './DebugPanel';
 import ResultPopup from './ResultPopup';
 import ShareButton from './ShareButton';
 import SpinButton from './SpinButton';
+import WheelIndicator from './WheelIndicator';
 import {
   isDuplicateChoice,
   createChoice,
@@ -330,17 +331,7 @@ const WheelSpinner = () => {
         {/* Wheel Section */}
         <div className="flex flex-col items-center gap-6 md:w-1/2 md:pr-8">
           <div className="relative w-full max-w-[480px] aspect-square mx-auto">
-            <div
-              className="absolute top-1/2 right-[-16px] -translate-y-1/2 w-0 h-0 
-              border-t-[15px] border-t-transparent 
-              border-b-[15px] border-b-transparent 
-              border-r-[30px] border-r-rose-500
-              filter drop-shadow-md z-10
-              before:absolute before:content-[''] before:right-[-32px] before:top-[-17px] 
-              before:border-t-[17px] before:border-t-transparent 
-              before:border-b-[17px] before:border-b-transparent 
-              before:border-r-[34px] before:border-r-white/50"
-            ></div>
+            <WheelIndicator />
             <div
               ref={wheelRef}
               className="w-full h-full rounded-full overflow-hidden relative shadow-lg will-change-transform"
